3c49acf0b140d265e36d979ac90cc7e8db552c9b
mauryadeepakprettify <mauryadeepak.prettify@gmail.com>
2026-01-07 16:35:06 +0530

    feat(home): ui

--- src/store/provider.js
+++ src/store/provider.js
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 const ReduxProvider = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return <Provider store={store}>{children} </Provider>;
 };
 
 export default ReduxProvider;
