08c1f071a6f5bad75e70e91f13fc3d6ef87f78ee
mauryadeepakprettify <mauryadeepak.prettify@gmail.com>
2026-01-07 16:51:22 +0530

    chore: setup

--- src/components/molecules/OverlayModal.jsx
+++ src/components/molecules/OverlayModal.jsx
@@ -10,13 +10,12 @@ const OverlayModal = ({ content, className }) => {
       className={`fixed inset-0 z-50 flex items-center justify-center bg-black/50 transition-all duration-500 ease-in-out ${isModal ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}
     >
       <div
-        className={`relative bg-[#0C1739] transition-all duration-500 ease-in-out ${className}`}
+        className={`relative bg-[#0C1739] transition-all  duration-500 ease-in-out ${className}`}
         onClick={(e) => e.stopPropagation()}
       >
         {content}
         <button
           className="absolute top-5 right-5"
-          onClick={() => setIsModal(false)}
         >
           <Image src="/icons/close.svg" alt="close" width={24} height={32} />
         </button>
